f4056e8b70dbcdc291547bf6bcda0a1869b5c42d
anandbk1 <41463910+anandbk1@users.noreply.github.com>
2019-06-30 09:11:35 +0530

    Update jenkinssetup.sh

--- jenkinssetup.sh
+++ jenkinssetup.sh
@@ -1,7 +1,8 @@
 #Install Java
 sudo apt-get update
 sudo apt install openjdk-8-jdk -y
-echo "JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/environment
+sudo chmod 777 /etc/environment
+sudo echo "JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/environment
 sudo source /etc/environment
 #
 echo $JAVA_HOME
@@ -23,7 +24,7 @@ mvn --version
 #
 #Install jenkins
 sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 sudo apt-get update
 sudo apt-get install jenkins -y
 sudo systemctl start jenkins
