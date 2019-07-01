ec0ce32f5d15eb2247e047a25a333a489ad00cdb
anandbk1 <41463910+anandbk1@users.noreply.github.com>
2019-07-01 15:14:29 +0530

    Update jenkinssetup.sh

--- jenkinssetup.sh
+++ jenkinssetup.sh
@@ -1,9 +1,19 @@
+# Install Java
 sudo apt update
 sudo apt install openjdk-8-jre-headless -y
 java -version
+#
+#Install Jenkins
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 sudo apt install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl status jenkins
+#
+#Install Maven
+sudo apt update
+sudo apt install maven -y
+mvn -version
+# to execute this script use below command
+#curl -sSL https://raw.githubusercontent.com/itrainspartans/itraindevops/master/jenkinssetup.sh | sh
