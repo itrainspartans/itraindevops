62980237186198ba34740185fbe2a577ca367bd7
anandbk1 <41463910+anandbk1@users.noreply.github.com>
2019-07-01 14:48:39 +0530

    Update jenkinssetup.sh

--- jenkinssetup.sh
+++ jenkinssetup.sh
@@ -1,31 +1,9 @@
-#Install Java
-sudo apt-get update
-sudo apt install openjdk-8-jdk -y
-sudo chmod 777 /etc/environment
-sudo echo "JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/environment
-sudo source /etc/environment
-#
-echo $JAVA_HOME
-#Install maven
-sudo apt-get update -y
-sudo apt-get upgrade -y
-cd /opt/
-sudo wget https://www-eu.apache.org/dist/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
-sudo tar -xvzf apache-maven-3.6.1-bin.tar.gz
-sudo mv apache-maven-3.6.1 maven
-sudo rm apache-maven-3.6.1-bin.tar.gz
-sudo touch /etc/profile.d/mavenenv.sh
-sudo chmod 777 /etc/profile.d/mavenenv.sh
-sudo echo "export M2_HOME=/opt/maven" >> /etc/profile.d/mavenenv.sh
-sudo echo "export PATH="$"{M2_HOME}/bin:"$"{PATH}" >> /etc/profile.d/mavenenv.sh
-source /etc/profile.d/mavenenv.sh
-mvn --version
-#
-#Install jenkins
-sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
-sudo apt-get update
-sudo apt-get install jenkins -y
+sudo apt update
+sudo apt install openjdk-8-jre-headless
+java -version
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
