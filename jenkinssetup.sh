b885e6f3a3910cb4fdee3d140229c7cc3b3291c7
anandbk1 <41463910+anandbk1@users.noreply.github.com>
2019-07-01 14:53:04 +0530

    Update jenkinssetup.sh

--- jenkinssetup.sh
+++ jenkinssetup.sh
@@ -1,9 +1,9 @@
 sudo apt update
-sudo apt install openjdk-8-jre-headless
+sudo apt install openjdk-8-jre-headless -y
 java -version
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
-sudo apt install jenkins
+sudo apt install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl status jenkins
